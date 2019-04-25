b34c55863fb295f930162e2c8a770e8788560371
trinabal <kmb6784@psu.edu>
2019-04-25 13:35:23 -0400

    Changed test.java formatting

--- test/test/src/test/Test.java
+++ test/test/src/test/Test.java
@@ -1,40 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-/**
- *
- * @author Suhyeok Bang
- */
+
 public class Test {
 
-
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
       BufferedReader objReader = null;
-  try
-  {
-   String commandLinetxt;
-   objReader = new BufferedReader(new FileReader("D:\\fileToRead.txt));
+      try
+      {
+          String commandLinetxt;
+          objReader = new BufferedReader(new FileReader("src/fileToRead.txt"));
 
-   while ((commandLinetxt = objReader.readLine()) != null) {
+          while ((commandLinetxt = objReader.readLine()) != null) {
+          System.out.println(commandLinetxt);
+          }
 
-    System.out.println(commandLinetxt);
-   }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
   }
-
-  } catch (IOException e) {
-
-   e.printStackTrace();
-
-      }
-
-  }
+}
